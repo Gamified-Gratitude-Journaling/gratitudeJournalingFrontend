c1318590d66ebd81c77ebaf13a848325c8ce649c
K12573 <75595156+K12573@users.noreply.github.com>
2021-12-31 15:39:03 -0500

    polished aboutPage UI

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,6 +2,8 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 import '../index.css';
 import gamePad from '../pages/images/gamePad.png';
+import tasks from '../pages/images/tasks.png';
+import interview from '../pages/images/interview.png';
 
 export default function About() {
 	// return (<div className="grid space-y-2 px-10 text-justify">
@@ -48,7 +50,7 @@ export default function About() {
 				<p className = 'font-semibold title text-center'>About the project</p>
 
 				<div className='aboutPage mt-5'>
-					<p className='aboutText flex items-center'>
+					<p className='aboutText pt-5'>
 						Hi! I'm Ray Wang! For my Capstone project, I'm researching how adding game elements to a digital gratitude journaling application impacts mental
 						well-being and engagement!
 
@@ -65,27 +67,64 @@ export default function About() {
 
 					</p>
 
-					<img className = 'mx-auto p-5' src={gamePad}/>
+					<img className = 'mx-auto pt-5' src={gamePad}/>
 				</div>
 				
 			</div>
 
-			<div className = 'h-screen bg-yellow-200 p-5' data-AOS='fade-up' data-AOS-duration='2000'>
+			<div className = 'h-full bg-yellow-200 p-5' data-AOS='fade-up' data-AOS-duration='2000'>
 				<p className = 'font-semibold title text-center'> Your Task</p>
+				
+				<div className = 'aboutPage mt-5'>
+					<p className='aboutText p-5'>
+						Make sure you register an account with your school email address!
 
-				<p className = 'mt-5'>
-					Make sure you register an account with your school email address!
+						<br/>
+						<br/>
 
-					<br/>
-					<br/>
+						Over the next 4 weeks, it would be grateful if you could use the app at least one every other day.
 
-					Over the next 4 weeks, it would be grateful if you could use the app at least one every other day.
+						<br/>
+						<br/>
 
-					<br/>
-					<br/>
+						<p className='aboutText font-semibold'>
+							At minium:
+						</p>
 
-					
-				</p>
+						<ul className="aboutText list-decimal px-10">
+							<li>Spend at least 1 minute on the online writing task every other day</li>
+							<li>Complete a 1-minute survey at the end of each week</li>
+							<li>Complete a 10-minute survey every two-weeks.</li>
+						</ul>
+
+						<br/>
+
+						<p className='aboutText'>
+							Otherwise! Be feel free to explore the gratefulness of the Grateful Journal!!
+						</p>
+					</p>
+						
+
+
+					<img className = 'mx-auto pt-5' src = {tasks}/>
+
+				</div>
+				
+			</div>
+
+			<div  className = 'h-full bg-yellow-500 p-5' data-AOS='fade-up' data-AOS-duration='2000'>
+				<p className = 'font-semibold text-center'>After 4 weeks</p>
+
+				<div className='aboutPage mt-5'>
+					<p className='aboutText p-5'>
+						You will receive 2 service hours!!
+						<br/>
+						<br/>
+						Additionally, I'd like to interview 4 random participants. The interview will take about 30 to 45 minutes each. 
+						TO reiterate the informed consent document, your participation in this interview and/or in this project is completely optional
+					</p>
+					<img className = 'mx-auto p-5'src={interview}/>
+				</div>
 			</div>
 		</div>
 	)
