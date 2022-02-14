11a8efc947739af16863542a764ddc8124e81c85
raywang999 <raywang999@gmail.com>
2022-02-14 16:16:46 -0500

    Remove treatment

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -25,23 +25,6 @@ const LOGIN_QUERY = gql`
 	}
 `;
 
-const treatmentEmails = [
-	"2022157@appleby.on.ca",
-	"2024178@appleby.on.ca",
-	"2023008@appleby.on.ca",
-	"2023026@appleby.on.ca",
-	"2025137@appleby.on.ca",
-	"2025073@appleby.on.ca",
-	"2022172@appleby.on.ca",
-	"2022079@appleby.on.ca",
-	"2024184@appleby.on.ca",
-	"2025165@appleby.on.ca",
-	"2024070@appleby.on.ca",
-	"2024009@appleby.on.ca",
-	"2024032@appleby.on.ca",
-	//"raywang999@gmail.com",
-];
-
 const isTreatment = (email) => {
 	let res = false;
 	treatmentEmails.forEach(treat => { if (email.localeCompare(treat) === 0) res = true; });
